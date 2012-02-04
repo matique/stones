c64adff7e3ecc23c31daa42e6e8b911145a71ff1
matique <dittmar.krall@matique.de>
2012-02-04 17:09:19 +0100

    'added concerned'

---
+++ lib/concerns.rb
@@ -0,0 +1,19 @@
+class << ActiveRecord::Base
+
+  def concerned_with(*concerns)
+    concerns.each do |concern|
+      require_dependency "#{name.underscore}/#{concern}"
+    end
+  end
+
+end
+
+class << ActionController::Base
+
+  def concerned_with(*concerns)
+    concerns.each do |concern|
+      require_dependency "#{name.underscore}/#{concern}"
+    end
+  end
+
+end
