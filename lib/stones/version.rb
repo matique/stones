22747b95bfb5cbbc5c638042f36302cf01ca6b0b
matique <dittmar.krall@matique.de>
2018-10-29 18:28:53 +0100

    'v1.0.6; three columns for .../layouts/_footer.slim'

--- lib/stones/version.rb
+++ lib/stones/version.rb
@@ -1,5 +1,6 @@
 module Stones
-  VERSION = '1.0.5'     # 2018-09-09
+  VERSION = '1.0.6'     # 2018-10-29
+#  VERSION = '1.0.5'     # 2018-09-09
 #  VERSION = '1.0.4'     # 2018-08-06
 #  VERSION = '1.0.3'
 end
